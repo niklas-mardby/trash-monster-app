d16771dfbd20da9e199c7e08cab8b3c3bae4ee2a
Niklas Mårdby <niklas.mardby@gmail.com>
2024-10-14 16:02:19 +0200

    Made array name plural

--- public/modules/mosterData.js
+++ public/modules/mosterData.js
@@ -1,5 +1,5 @@
 export const monsterData = {
 	colors: ["purple", "pink", "skyblue", "limegreen"],
 	types: ["cute", "evil"],
-	description: ["friends", "minions", "cuteness level"],
+	descriptions: ["friends", "minions", "cuteness level"],
 };
